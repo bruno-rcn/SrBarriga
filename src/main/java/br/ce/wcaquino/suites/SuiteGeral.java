d0358b36e0d4513b672abe207abe61210f4dc7cf
Bruno Noberto <brunorcnoberto@hotmail.com>
2021-03-23 15:28:44 -0300

    teste resumo mensal vazio

--- src/main/java/br/ce/wcaquino/suites/SuiteGeral.java
+++ src/main/java/br/ce/wcaquino/suites/SuiteGeral.java
@@ -1,7 +1,6 @@
 package br.ce.wcaquino.suites;
 
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
