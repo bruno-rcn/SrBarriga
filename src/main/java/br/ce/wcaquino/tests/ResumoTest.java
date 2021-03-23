9539c00cfddbe7cd8b94005479b03ab3e8857fe7
Bruno Noberto <brunorcnoberto@hotmail.com>
2021-03-23 14:56:18 -0300

    resumo mensal

--- src/main/java/br/ce/wcaquino/tests/ResumoTest.java
+++ src/main/java/br/ce/wcaquino/tests/ResumoTest.java
@@ -1,9 +1,12 @@
 package br.ce.wcaquino.tests;
 
+import static br.ce.wcaquino.core.DriverFactory.getDriver;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import br.ce.wcaquino.core.BaseTest;
+import br.ce.wcaquino.core.DriverFactory;
 import br.ce.wcaquino.pages.MenuPage;
 import br.ce.wcaquino.pages.ResumoPage;
 
@@ -19,5 +22,12 @@ public class ResumoTest extends BaseTest {
 		
 		Assert.assertEquals("Movimentação removida com sucesso!",
 				resumoPage.obterMensagemSucesso());
+	}
+	
+	@Test
+	public void testResumoMensal(){
+		menuPage.acessarTelaResumo();
+		
+		Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
 	}
 }
