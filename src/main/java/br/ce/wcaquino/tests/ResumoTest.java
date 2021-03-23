d1ecc3bd0b834c31ba5b204248dfdcda1fa83a4d
Bruno Noberto <brunorcnoberto@hotmail.com>
2021-03-23 14:43:59 -0300

    teste: remover movimentacao

---
+++ src/main/java/br/ce/wcaquino/tests/ResumoTest.java
@@ -0,0 +1,23 @@
+package br.ce.wcaquino.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.core.BaseTest;
+import br.ce.wcaquino.pages.MenuPage;
+import br.ce.wcaquino.pages.ResumoPage;
+
+public class ResumoTest extends BaseTest {
+	private MenuPage menuPage = new MenuPage();
+	private ResumoPage resumoPage = new ResumoPage();
+
+	@Test
+	public void testExcluirMovimentacao(){
+		menuPage.acessarTelaResumo();
+		
+		resumoPage.excluirMovimentacao();
+		
+		Assert.assertEquals("Movimentação removida com sucesso!",
+				resumoPage.obterMensagemSucesso());
+	}
+}
